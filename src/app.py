1165e9831df10e5a6d235db057724d404e8c19d6
WilliamAdamsWAG <williamadams.aurora@gmail.com>
2024-08-20 14:19:13 +0300

    annotations and comments have been updated, update class App

--- src/app.py
+++ src/app.py
@@ -1,39 +1,46 @@
+import os
+
 from aiogram import Dispatcher, Bot
 from aiogram.types import BotCommand
 
 from triggers.commands.start import start_command
 from triggers.messages.echo_message import echo_message
 
-from backend.core import Core
 from backend.log import Log
 from backend.templates import Templates
 
 class App:
-    dispatcher = Dispatcher()
-    core = Core()
-    log = Log()
+    """ The base class of the bot, which describes the parameters of its launch """
+    TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
     
     def __init__(self):
-        self.bot = Bot(token=self.core.token)
+        self.bot = Bot(token=self.TOKEN)
+        self.dispatcher = Dispatcher()
         
     async def configurate(self):
+        """ Configuring and connecting components """
         await self.set_commands()
-        await self.register_handlers()
+        await self.connect_triggers()
     
     async def set_commands(self):
+        """ Enable commands to menu """
         await self.bot.set_my_commands(commands=[
-            BotCommand(command="/start", description="Начать работу"),
+            BotCommand(command="/start", description="greeting"),
         ])
         
-    async def register_handlers(self):
-        handlers = (
+    async def connect_triggers(self):
+        """ Connect routers for enable triggers """
+        # IMPORTANT: Messages and activity in the bot goes through this list sequentially, 
+        # so you need to properly arrange connections, because if 1 handler is triggered, 
+        # the second one will not run
+        triggers = (
             start_command,
             echo_message,
         )
 
         log_message: str = f"\n{' ROUTERS '.center(48, '=')}\n"
 
-        for index, router in enumerate(handlers):
+        for index, router in enumerate(triggers):
             try:
                 self.dispatcher.include_router(router)
                 log_message += f"{router.name:<20}[{index+1}] initialized success ✅\n"
@@ -42,10 +49,12 @@ class App:
 
         log_message += f"{''.center(48, '=')}\n"
 
-        self.log.bot_logging(log_message)
+        Log.bot_logging(log_message)
         
     async def start(self):
-        self.log.bot_logging(Templates.LOG_BOT_POLLING)
+        Log.bot_logging(Templates.LOG_BOT_POLLING)
         
+        # When the bot is turned off all commands from users are saved and will be processed at startup, 
+        # in order not to process old requests you need to delete webhooks
         await self.bot.delete_webhook(drop_pending_updates=True)
-        await self.dispatcher.start_polling(self.bot)
+        await self.dispatcher.start_polling(self.bot) # Run bot
