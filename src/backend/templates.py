0a481c7c5db77846abcb17e529483c223d410a80
WilliamAdamsWAG <williamadams.aurora@gmail.com>
2024-08-20 21:50:46 +0300

    new logging templates

--- src/backend/templates.py
+++ src/backend/templates.py
@@ -3,14 +3,21 @@ from datetime import datetime
 
 
 class Templates:
-    LOG_FORMAT = "\n{level.icon}{time:DD.MM HH:mm:ss}\n{message}\n"
+    LOG_FORMAT: str = "\n{level.icon}{time:DD.MM HH:mm:ss}\n{message}\n"
     
-    LOG_BOT_POLLING = (f"\n{' POLLING '.center(50, '=')}\n"
-                   f"Bot polling started at {datetime.now()}\n"
-                   f"{''.center(50, '=')}\n")
+    LOG_BOT_POLLING: str = (f"\n{' POLLING '.center(50, '=')}\n"
+                       f"Bot polling started at {datetime.now()}\n"
+                       f"{''.center(50, '=')}\n")
     
     LOG_MESSAGE = Template(f"\nMessage ->\n"
                            f"@$username:$user_id (chat $chat)\n"
                            f"$text\n")
     
-    START = "This is example of greeting text"
+    LOG_ROUTERS_REGISTR = Template(f"\n{' ROUTERS '.center(48, '=')}\n"
+                                   f"$routers"
+                                   f"{''.center(48, '=')}\n")
+     
+    LOG_ROUTER_TRUE = Template(f"$router[$index] initialized success ✅\n")
+    LOG_ROUTER_FALSE = Template(f"$router[$index] failed ❌\n")
+    
+    START: str = "This is example of greeting text"
