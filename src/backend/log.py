2b46f2c3fea00f47da80215f948c9c933b12909c
WilliamAdamsWAG <williamadams.aurora@gmail.com>
2024-08-20 13:05:45 +0300

    update Log class

--- src/backend/log.py
+++ src/backend/log.py
@@ -3,24 +3,28 @@ from loguru import logger
 from backend.templates import Templates
 
 logger.add("../logs/bot.log",
-           format="\n{level.icon}{time:DD.MM HH:mm:ss}\n{message}\n",
+           format=Templates.LOG_FORMAT,
            level="DEBUG",
            filter=lambda record: record['extra'].get('name') == f'bot')
 
 logger.add("../logs/messages.log",
-           format="\n{level.icon}{time:DD.MM HH:mm:ss}\n{message}\n",
+           format=Templates.LOG_FORMAT,
            level="DEBUG",
            filter=lambda record: record['extra'].get('name') == f'message')
 
-class Log:
-    bot_log = logger.bind(name="bot")
-    message_log = logger.bind(name="message")
+BOT_LOG = logger.bind(name="bot")
+MESSAGE_LOG = logger.bind(name="message")
 
-    def bot_logging(self, text: str) -> None:
-        self.bot_log.info(text)
-        
-    def message_logging(self, *, call: str, user, chat_id):
-        self.message_log.info(Templates.LOG_MESSAGE.substitute(
+class Log:
+    @staticmethod
+    def bot_logging(text: str) -> None:
+        """ Log bot activities """
+        BOT_LOG.info(text)
+    
+    @staticmethod
+    def message_logging(*, call: str, user, chat_id):
+        """ Log messages """
+        MESSAGE_LOG.info(Templates.LOG_MESSAGE.substitute(
             user_id=user.user_id,
             username=user.username,
             chat=chat_id,
