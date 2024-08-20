65bfd3487c514eaeae327a0d67da5d02d96372e9
WilliamAdamsWAG <williamadams.aurora@gmail.com>
2024-08-20 12:09:20 +0300

    both log template

--- src/backend/templates.py
+++ src/backend/templates.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 
 class Templates:
+    LOG_FORMAT = "\n{level.icon}{time:DD.MM HH:mm:ss}\n{message}\n"
+    
     LOG_BOT_POLLING = (f"\n{' POLLING '.center(50, '=')}\n"
                    f"Bot polling started at {datetime.now()}\n"
                    f"{''.center(50, '=')}\n")
